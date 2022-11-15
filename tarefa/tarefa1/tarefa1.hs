57ce0dfdff31984c8f93bafccde9ce31a0a27517
A13 <A13@MBP-de-Sandra.home>
2022-11-15 01:28:42 +0100

    tarefa

---
+++ tarefa/tarefa1/tarefa1.hs
@@ -0,0 +1,17 @@
+
+-- Question 1
+-- Write a multiline comment below.
+add6numbers u v w x y z = u + v + w + x + y + z
+
+-- Question 2
+-- Define a function that takes a value and multiplies it by 3.
+
+-- Question 3
+-- Define a function that calculates the area of a circle.
+
+-- Question 4
+-- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
+
+-- Question 5
+-- Define a function that takes the height and radius of a cylinder and checks if the volume is greater than or equal to 42.
+
